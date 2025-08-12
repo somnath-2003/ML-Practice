fe030861a8b8cb331fbdf78808ff20f4fab8a26f
Somnath Adhikari <somnathadhikari2003@gmail.com>
2025-08-13 01:25:31 +0530

    feat: add NumPy shape, reshape, flatten, and ravel examples

---
+++ Numpy/array_operation.py
@@ -0,0 +1,75 @@
+import numpy as np
+a=np.array([1,2,3,4,5])#1D array
+print(a)
+print("Datatype of a: ",a.dtype)
+b= np.array([[1,2,3,4],[5,6,7,8]])#2D array
+print("Datatype of b: ",b.dtype)
+
+c=np.array([[[1,2],[3,4]],[[5,6],[7,8]]])
+print(c)
+print(f"dimention of c: {c.ndim} Shape is:{c.shape} \n Data type of c:{c.dtype}")
+#shape=(2,2,2)means two block each have 2 row 2 col
+#print dimention
+print("Dimention of a: ",a.ndim)
+print("Dimention of b: ",b.ndim)
+
+#shape
+print("Shape(row*col of array) of a : ",a.shape)
+print("Shape(order of the array) of b : ",b.shape)
+
+#Float array
+arr1=np.array([1.2,3.4,6.6])
+print('Data type of arr1: ',arr1.dtype)
+#force conversion of datatypes
+arr2=arr1.astype(np.float32)
+print("Data Type of arr2 :",arr2.dtype)
+#float to int
+arr3=arr1.astype(np.int32)
+print(arr3)
+print("datatype of arr3 : ",arr3.dtype)
+#To get round off values from floating points
+arr3=np.rint(arr1).astype(np.int32)
+print(arr3)
+
+#create differnt arrys
+#filled with zeros
+arr4=np.zeros((2,3))
+print("arr4 \n:",arr4)
+#filled with 1
+arr5=np.ones((2,3))
+print("arr5 \n:",arr5)
+#filled with random garbage value
+arr6=np.empty((2,3))
+print("arr6 :\n",arr6)
+
+#creat array from range
+arr7=np.arange(10)
+print("arr7:\n",arr7)
+
+#reshape array
+arr8=np.reshape(arr7,(2,5))#or you can use arr8=arr7.reshape(2,5)
+print("arr8 :\n",arr8)
+print(arr8.ndim)
+
+#Using -1 for auto-calculation
+arr9 = np.arange(12).reshape(3, -1)
+print("\nAuto shaped arr9: (-1):\n", arr9)
+
+#for higher dimensions
+arr10 = np.arange(24)  # total elements = 24
+
+reshaped = arr10.reshape(2, 3, -1)  # Let NumPy figure out last dimension
+print(reshaped.shape)  
+print(reshaped)
+
+#Flattening of Multidimentional Arrays
+#Flattening means turning a multi-dimensional array into a 1D array
+arr11=np.array([[2,5,6],[7,3,1]])
+flat_copy=arr11.flatten()
+print("Flatted copy is \n",flat_copy)
+
+# .ravel() also converts a multi-dimensional array into a 1D array, just like .flatten().
+flat_view = arr11.ravel()
+flat_view[0] = 99
+print(flat_view)  # [2,5,6,7,3,1]
+print(arr11)        # [[99,5,6] [ 7,3,1]] 
